e9d508ac964d5a126267a8fa183609b4c4f827af
Anna Abbott <aabbott@byupathway.edu>
2025-12-12 16:14:14 -0600

    Completed Course Information, step 1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,12 +1,38 @@
-const Hello = () => {
-  return <div>Hello world</div>;
+import React from "react";
+
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Content = (props) => {
+  return (
+    <p>
+      {props.part1} {props.exercise1}
+      {props.part2} {props.exercise2}
+      {props.part3} {props.exercise3}
+    </p>
+  );
+};
+
+const Total = (props) => {
+  return <p>Number of exercises: {props.total}</p>;
 };
 
 const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercise1 = 10;
+  const part2 = "Using Props to Pass Data";
+  const exercise2 = 7;
+  const part3 = "State of a Component";
+  const exercise3 = 14;
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello />
+      <Header course={course} />
+      <Content part1={part1} exercise1={exercise1} />
+      <Content part2={part2} exercise2={exercise2} />
+      <Content part3={part3} exercise3={exercise3} />
+      <Total total={exercise1 + exercise2 + exercise3} />
     </div>
   );
 };
